e216acddfd7fa9042e85456579da448e1bfda637
Abhik Salian <21e03.abhik@sjec.ac.in>
2024-10-14 21:16:10 +0530

    key points being detected

---
+++ posture-detection/baby_movement_detection.py
@@ -0,0 +1,17 @@
+import cv2
+import mediapipe as mp
+mp_pose = mp.solutions.pose
+mp_drawing = mp.solutions.drawing_utils
+pose = mp_pose.Pose()
+cap = cv2.VideoCapture(0)
+while True:
+    ret, frame = cap.read()
+    image_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    results = pose.process(image_rgb)
+    if results.pose_landmarks:
+        mp_drawing.draw_landmarks(frame, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+    cv2.imshow('Baby Movement Detection', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
